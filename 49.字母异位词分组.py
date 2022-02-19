e18c9942e7835780b22781f8bc201bd67ee473db
fmbao <1689940525@qq.com>
2022-02-19 12:25:15 +0800

    2022.2.19 update3

---
+++ 49.字母异位词分组.py
@@ -0,0 +1,18 @@
+
+#
+# @lc app=leetcode.cn id=49 lang=python3
+#
+# [49] 字母异位词分组
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = collections.defaultdict(list)
+        for st in strs:
+            key = "".join(sorted(st))
+            mp[key].append(st)
+        return list(mp.values())
+
+# @lc code=end
+
